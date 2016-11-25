716743d3339546d1bb960338ebfe2ec737d7acf6
Picpol <github@lindbuechl.eu>
2016-11-25 15:09:33 +0100

    Konfigurationsbeispiel
    Beispieldefinition der steuerbaren Geräte

---
+++ amazonEchoSampleAppliances.js
@@ -0,0 +1,12 @@
+[
+  {"room" : "Küche", "appliances" : [{"name" : "Deckenleuchte", "onOff" : "1/2/3"}]},
+  {"room" : "Wohnzimmer", "appliances" : [ 
+    {"name" : "Deckenleuchte", "onOff":"1/2/4", "percent":"1/2/5"},
+    {"name" : "Stehleuchte", "onOff":"1/2/6", "percent":"1/2/7", "aliases" : ["Stehlampe Wohnzimmer"]},
+    {"name" : "Vitrine", "onOff":"1/2/8", "aliases" : ["Vitrinenbeleuchtung Wohnzimmer"]}
+  ]},
+  {"room" : "Lichtszene", "appliances" : [
+    {"name" : "Essen", "onOff":"1/3/1", "value":"0"},
+    {"name" : "Deko", "onOff":"1/3/1", "value":"1"}
+  ]}
+]
